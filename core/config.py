d86b88eebfc371868a13c01f480e81ed06b07ea8
Гена Иванов <hena300404@gmail.com>
2026-06-08 11:48:45 +0300

    main.py не полный+конфиг токен бота

--- core/config.py
+++ core/config.py
@@ -4,6 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env")
 
+    bot_token: str
     database_url: str
     redis_url: str
     api_base_url: str = "http://api:8000"
